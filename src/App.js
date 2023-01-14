25847717b2b294076f6becf258026d273da65f95
saniyasaher20 <saniyasaher20@gmail.com>
2023-01-14 09:01:38 +0530

    components created

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const AppLayout = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render()
