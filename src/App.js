6684bff963d11903609dd725a19d16ba5816ca5e
saniyasaher20 <saniyasaher20@gmail.com>
2023-02-12 23:11:17 +0530

    Deploying

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 
 const AppLayout = () => {
-    // Lifting the state up, so that Body and Header>Search component can communicate
 
     return (
         <div className="m-x">
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import { RESTAURANT_API } from "../constants";
 import Search from "./Search";
 
 const Body = () => {
@@ -7,30 +8,33 @@ const Body = () => {
     const [allRestaurants, setAllRestaurants] = useState([]);
     const [filteredRestaurants, setFilteredRestaurants] = useState([]);
 
+    console.log(RESTAURANT_API)
     // Fetch restaurant data via API
     async function getRestaurants() {
-        // const data = await fetch(RESTAURANT_API);
-        // const json = await data.json();
-        const restaurantData = await import("../api.json");
-        // console.log(restaurantData?.data?.cards[0]?.data?.data?.cards)
-        setAllRestaurants(restaurantData?.data?.cards[0]?.data?.data?.cards)
-        setFilteredRestaurants(restaurantData?.data?.cards[0]?.data?.data?.cards)
+        const data = await fetch(RESTAURANT_API);
+        const json = await data.json();
+        // const json = await import("../api.json"); // offline api, change -> cards[0] 
+        // console.log(json?.data?.cards[0]?.data?.data?.cards)
+
+        setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+        setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards)
     }
 
     useEffect(() => {
         getRestaurants();
     }, [])
 
+    // early return
     if (!allRestaurants) return null;
 
     return (
         <>
             <section className="bg-gradient-to-b from-orange-700 to-red-800 py-10">
-            <div className="container w-2/4 py-6">
-                <Search
-                    allRestaurants={allRestaurants}
-                    setFilteredRestaurants={setFilteredRestaurants}
-                />
+                <div className="container w-2/4 py-6">
+                    <Search
+                        allRestaurants={allRestaurants}
+                        setFilteredRestaurants={setFilteredRestaurants}
+                    />
                 </div>
             </section>
             <div className="container mx-auto py-6 grid grid-cols-4 gap-8">
